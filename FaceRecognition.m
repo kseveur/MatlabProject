a057ece535926baa064696f61a2a8d740f1bcb52
kseveur <kseveur@hotmail.com>
2018-11-20 14:09:17 -0500

    Update FaceRecognition.m

--- FaceRecognition.m
+++ FaceRecognition.m
@@ -1,4 +1,7 @@
 %% Load Image info from Face Database Directory
+clc;
+clear;
+close all;
 faceDatabase = imageSet('FaceDatabaseFolder', 'recursive');
 
 
@@ -23,7 +26,7 @@ title('HOG Features');
 
 %% Extract HOG Features for Training Set
 % 4680 is used as the output from the HOG is 1 X 4680
-trainingFeatures = zeros(size(training,1) * training(1).Count,6826716);%4680);
+trainingFeatures = zeros(size(training,1) * training(1).Count,4680);% 6826716);
 featureCount = 1;
 for i = 1:size(training,2)
     for j = 1:training(i).Count
@@ -37,7 +40,7 @@ end
 
 
 %% Create classifier
-faceClassifier = fitcecoc(trainingFeatures, trainingLable);
+faceClassifier = fitcecoc(trainingFeatures, trainingLabel);
 
 
 %% Test One Image from Test Set
@@ -46,7 +49,7 @@ queryImage = read(test(person),1);  %This is where we need to insert our new ima
 queryFeatures = extractHOGFeatures(queryImage);
 personLabel = predict(faceClassifier, queryFeatures);
 % Go back to training set to find a match
-booleanIndex = strcmp(personalLabel, personIndex);
+booleanIndex = strcmp(personLabel, personIndex);
 integerIndex = find(booleanIndex);
 subplot(1,2,1);
 imshow(queryImage);
@@ -66,14 +69,17 @@ for person = 1:5
         queryFeatures = extractHOGFeatures(queryImage);
         personLabel = predict(faceClassifier, queryFeatures);
         % Go back to training set to find a match
-        booleanIndex = strcmp(personalLabel, personIndex);
+        booleanIndex = strcmp(personLabel, personIndex);
         integerIndex = find(booleanIndex);
         subplot(2,2,figureNum);
         imshow(imresize(queryImage,3));
         title('Query Face');
         
         subplot(2,2,figureNum + 1);
-        imshow(imresize(read(training(integerIndex)), 3));
+        imshow(imresize(read(training(integerIndex),1),3));
         title('Matched Class');
+        figureNum = figureNum+2;
     end
+    figure;
+    figureNum = 1;
 end
